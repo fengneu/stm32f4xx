c3764574b8fecd506badda184a1b97db98d08719
yejq <yeholmes@outlook.com>
2016-12-18 21:59:04 +0800

    Update driver source files from STM32LIB, v1.8.0

--- USER/main.c
+++ USER/main.c
@@ -1,8 +1,65 @@
-#include "usart.h"
+/*
+ * Modified by yeholmes@outlook.com
+ *
+ * General develop template for STM32F4xx
+ *
+ * Hangzhou, China, 2016/12/18
+ */
+
+#include <stdio.h>
+#include "stm32f4xx_conf.h"
+
+#pragma import(__use_no_semihosting)
+struct __FILE
+{
+	int handle;
+};
+
+FILE __stdout;
+void _sys_exit(int x)
+{
+	x = x;
+}
+
+int fputc(int ch, FILE *f)
+{
+	while ((USART1->SR & 0X40) == 0);
+	USART1->DR = (u8) ch;
+	return ch;
+}
+
+static void uart_init(u32 bound) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = bound;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+
+	USART_Cmd(USART1, ENABLE);
+	USART_ClearFlag(USART1, USART_FLAG_TC);
+}
 
 int main(void)
 {        
-	uart_init(115200);		//初始化串口波特率为115200
+	uart_init(115200);
 	printf("Applicate starts, compliation date & time: %s, %s\r\n", __DATE__, __TIME__);
 	for (;;) {
 	}
